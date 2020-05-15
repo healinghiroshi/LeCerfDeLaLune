60afc8193f6418757dc812248ee86e77c8d925e6
unknown <edwardy1011@gmail.com>
2020-05-15 16:49:53 -0700

    Uploading Files

---
+++ Assets/jumpScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpScript : MonoBehaviour
+{
+   
+    public float moveSpeed = 5f;
+    public bool isFloor = false;
+  
+
+    private SpriteRenderer mySpriteRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Right Jump") && isFloor == true)
+        {
+            mySpriteRenderer.flipX = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 5f), ForceMode2D.Impulse);
+        }
+        if (Input.GetButtonDown("Left Jump") && isFloor == true)
+        {
+            mySpriteRenderer.flipX = true;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, 5f), ForceMode2D.Impulse);
+        }
+    }
+
+    
+}
---
+++ Assets/stepScript.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class stepScript : MonoBehaviour
+{
+    public GameObject stepPrefab;
+    public GameObject nextStep;
+    public Vector3[] stepPosition;
+
+    public int numberOfSteps = 200;
+    public float levelWidth = 6f;
+    public float distanceX = 1f;
+    public float distanceY = 1f;
+
+   
+
+    void Start()
+    {
+        
+        Vector3 spawnPosition = new Vector3();
+        spawnPosition = GameObject.FindGameObjectWithTag("step").transform.position;
+        int x = 0;
+        for (int i = 0; i<numberOfSteps; i++)
+        {
+            
+            spawnPosition.y += distanceY;
+            spawnPosition.x = Random.Range(0,2)*2-1+spawnPosition.x;
+            if (spawnPosition.x >= 3)
+            {
+                spawnPosition.x = 0 - 2 + spawnPosition.x;
+                stepPosition = new Vector3[numberOfSteps];
+                nextStep = Instantiate(stepPrefab, spawnPosition, Quaternion.identity);
+                stepPosition[x] = spawnPosition;
+                x++;
+                spawnPosition = stepPosition[x - 1];
+            }
+            else if (spawnPosition.x <= -3)
+            {
+                spawnPosition.x = 2 + spawnPosition.x;
+                stepPosition = new Vector3[numberOfSteps];
+                nextStep = Instantiate(stepPrefab, spawnPosition, Quaternion.identity);
+                stepPosition[x] = spawnPosition;
+                x++;
+                spawnPosition = stepPosition[x - 1];
+            }
+            else
+            {
+                stepPosition = new Vector3[numberOfSteps];
+                nextStep = Instantiate(stepPrefab, spawnPosition, Quaternion.identity);
+                stepPosition[x] = spawnPosition;
+                x++;
+                spawnPosition = stepPosition[x - 1];
+            }
+            
+        }
+     
+    }
+
+    void Update()
+    {
+
+    }
+}
